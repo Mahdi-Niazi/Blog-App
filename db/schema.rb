c0bede781ee7bb3f28363dcc30930373b258c4af
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-26 21:39:01 +0430

    Add model for post and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_25_131537) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_170438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +44,27 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_25_131537) do
     t.index ["author_id"], name: "index_post_tables_on_author_id"
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.bigint "author_id"
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
   create_table "user_tables", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
     t.text "bio"
@@ -58,4 +78,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_25_131537) do
   add_foreign_key "like_tables", "post_tables", column: "post_id"
   add_foreign_key "like_tables", "user_tables", column: "author_id"
   add_foreign_key "post_tables", "user_tables", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
