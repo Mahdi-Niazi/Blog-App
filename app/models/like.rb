af63be64c5cda48fffb8aa4dede517e8f27eae59
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-27 08:53:00 +0430

    Add private to methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,6 +4,7 @@ class Like < ApplicationRecord
 
   after_save :update_like_counter
 
+  private
   def update_like_counter
     Post.find(post_id).update(likes_counter: Like.where(post_id).count)
   end
